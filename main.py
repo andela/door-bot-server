e8ad7fdd7569f85eb0d7e0d9b0670b96b7ea6390
andela-gukpere <godson.ukpere@andela.co>
2015-05-18 15:27:08 +0000

    feature(bot-server): Configured server endpoint with flask running port 7770

---
+++ main.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as GPIO
+import time
+from flask import Flask
+app = Flask(__name__)
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(18, GPIO.OUT)
+
+@app.route("/open/")
+def openDoor():
+        GPIO.output(18, 1)
+    	status = "Main Door is opened"
+    	return status
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=7770)
+GPIO.cleanup()
