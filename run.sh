1a022c2361ad5244a76d9abc1b9afc37460ca503
andela-gukpere <godson.ukpere@andela.co>
2015-05-18 15:42:54 +0000

    fix(shell) python and ngrok run async, after killing redundant tasks

--- run.sh
+++ run.sh
@@ -1,8 +1,17 @@
-python main.py
+# Kill python and ngrok instances
+sudo pkill python
+sudo pkill ngrok
+
+#run python server
+sudo python main.py >/dev/null 2>&1 &
+
 ngrok http 7770 >/dev/null 2>&1 &
+
 echo 'Waiting for ngrok to run'
+
 ## Sleep for 5 seconds, anticipating ngrok's completion
 sleep 5
+
 ## update our firebase object with the valid ngrok URL
 python get_public_addr.py
 echo 'Running on port 7770'
