944dc777bc49b08b5489ced3c92c85d58de83a3c
andela-gukpere <godson.ukpere@andela.co>
2015-05-18 16:13:57 +0100

    feature(shell script): The required proceedure has been deployed into the main shell script

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 pyhon main.py
-ngrok http 7770
+ngrok http 7770 >/dev/null 2>&1 &
+echo 'Waiting for ngrok to run'
+## Sleep for 5 seconds, anticipating ngrok's completion
+sleep 5
+## update our firebase object with the valid ngrok URL
 python get_public_addr.py
-
 echo 'Running on port 7770'
